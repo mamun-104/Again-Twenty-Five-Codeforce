2e65fd07dac9b583a025e7d89cfe0608583aac97
Md Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2023-03-23 10:58:39 +0600

    Add files via upload

---
+++ Again Twenty Five.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long  long int res=0,n;
+    string s;
+    cin>>n;
+
+        cout<<25<<"\n";
+
+
+
+    return 0;
+}
+
+
+
